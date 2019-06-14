4adde53dfb5be6f77619fa25e994d1f46e18493c
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2019-06-13 20:58:42 -0700

    Updated RPC tests

--- tests/containers/experimental/rpc/rpc_checksum1.cpp
+++ tests/containers/experimental/rpc/rpc_checksum1.cpp
@@ -6,42 +6,59 @@
 #include <bcl/bcl.hpp>
 #include <bcl/containers/experimental/rpc_checksum.hpp>
 
+template <typename Future>
+bool ready(std::vector<Future>& futures) {
+  for (auto& future : futures) {
+    if (future.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <typename Future>
+void future_barrier(std::vector<Future>& futures) {
+  bool success = false;
+  do {
+    BCL::flush_rpc();
+    size_t success_count = ready(futures);
+    success_count = BCL::allreduce<size_t>(success_count, std::plus<size_t>{});
+    success = success_count == BCL::nprocs();
+  } while (!success);
+}
+
 int main(int argc, char** argv) {
-//   BCL::init();
-//   BCL::init_rpc();
+  BCL::init();
+  BCL::init_rpc();
 
 
-//   auto fn = [](int a, int b) -> int {
-//                return a * b;
-//             };
-//   int a = 7;
-//   int b = 7;
+  auto fn = [](int a, int b) -> int {
+               return a * b;
+            };
+  int a = 7;
+  int b = 7;
 
 
-//   // basic buffered RPC test
-//   // if (BCL::rank() == 0) {
-//   for (int i = 0 ; i < 100; i++) {
-//     BCL::buffered_rpc(i % 2, fn, a, b);
-//   }
+  using rv = decltype(BCL::buffered_rpc(0, fn, a, b));
+  std::vector<rv> futures;
+  if (BCL::rank() == 1) {
+    for (int i = 0 ; i < 1000; i++) {
+      auto f = BCL::buffered_rpc(0, fn, a, b);
+      futures.push_back(std::move(f));
+    }
+  }
 
-//   BCL::barrier();
+  BCL::flush_signal(); 
 
-//   BCL::flush_signal();
-//   BCL::barrier();
-//     // BCL::buffered_rpc(1, fn, a, b);
-//     // BCL::buffered_rpc(1, fn, a, b);
-//     // BCL::buffered_rpc(1, fn, a, b);
-//   // }
+  future_barrier(futures);
 
-//   // BCL::flush_signal();
-//   // basic RPC test
-//   // rpc_t test_rpc(0);
-//   // test_rpc.load(fn, a, b);
-//   // run_rpc(test_rpc);
+  for (auto& f : futures) {
+    int val = f.get();
+    assert(val == a*b);
+  }
 
+  BCL::finalize_rpc();
+  BCL::finalize();
 
-
-//   BCL::finalize_rpc();
-//   BCL::finalize();
   return 0;
 }
