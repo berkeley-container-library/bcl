db44d3be43dcebdb25bc1813ea34df624c72da78
Benjamin Brock <brock@cs.berkeley.edu>
2019-07-26 18:03:42 -0500

    Add atomic insert benchmark

---
+++ examples/benchmarks/am-comp/data-structures/hash-table/am-hash-insert.cpp
@@ -0,0 +1,42 @@
+#include <bcl/bcl.hpp>
+#include <queue>
+
+std::unordered_map<int, int> map;
+
+int main(int argc, char** argv) {
+  BCL::init();
+
+  BCL::gas::init_am();
+
+  auto insert = BCL::gas::register_am([](int key, int value) -> void {
+    map[key] = value;
+  }, int(), int());
+
+  size_t num_ams = 100000;
+  size_t key_space = 1000000;
+  size_t keys_per_n = (key_space + BCL::nprocs() - 1) / BCL::nprocs();
+
+  srand48(BCL::rank());
+  BCL::barrier();
+  auto begin = std::chrono::high_resolution_clock::now();
+
+  for (size_t i = 0; i < num_ams; i++) {
+    size_t value = lrand48() % key_space;
+    size_t remote_proc = value / keys_per_n;
+
+    insert.launch(remote_proc, value, value);
+    BCL::gas::flush_am();
+  }
+  
+  BCL::barrier();
+  auto end = std::chrono::high_resolution_clock::now();
+  double duration = std::chrono::duration<double>(end - begin).count();
+
+  double duration_us = 1e6*duration;
+  double latency_us = duration_us / num_ams;
+
+  BCL::print("Latency is %lf us per AM. (Finished in %lf s)\n", latency_us, duration);
+
+  BCL::finalize();
+  return 0;
+}
