c6f0694d19e6a0dc8df2873d2fa7f98ce873eb3b
Jiakun Yan <jiakunyan1998@gmail.com>
2019-08-21 14:29:04 -0700

    attentiveness benchmark

---
+++ examples/benchmarks/am-comp/attentiveness/bcl-attentiveness-queue-insert.cpp
@@ -0,0 +1,62 @@
+#include <bcl/bcl.hpp>
+#include <bcl/containers/FastQueue.hpp>
+
+void compute_by_time(double time_us);
+void compute_by_work(double time_us);
+
+int main(int argc, char** argv) {
+  size_t num_ops = 100000;
+  double compute_array[] = {0, 0, 0.5, 1, 2, 4, 8, 16, 32};
+  BCL::init(16);
+
+  for (auto compute_us : compute_array) {
+    std::vector<BCL::FastQueue<int>> queues;
+
+    for (size_t rank = 0; rank < BCL::nprocs(); rank++) {
+      queues.push_back(BCL::FastQueue<int>(rank, num_ops*2));
+    }
+
+    srand48(BCL::rank());
+    BCL::barrier();
+
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    for (size_t i = 0; i < num_ops; i++) {
+      size_t remote_proc = lrand48() % BCL::nprocs();
+
+      queues[remote_proc].push(BCL::rank());
+
+      compute_by_time(compute_us);
+      // usleep(compute_us);
+    }
+
+    BCL::barrier();
+    auto end = std::chrono::high_resolution_clock::now();
+    double duration = std::chrono::duration<double>(end - begin).count();
+
+    double duration_us = 1e6*duration;
+
+    double latency_us = (duration_us - compute_us*num_ops) / num_ops;
+
+    BCL::print("Latency is %lf us per op. compute time is %lf us per op. (Finished in %lf s)\n",
+               latency_us, compute_us, duration);
+  }
+
+  BCL::finalize();
+  return 0;
+}
+
+void compute_by_time(double time_us) {
+  double time = time_us / 1e6;
+  auto begin = std::chrono::high_resolution_clock::now();
+  auto now = begin;
+  double duration = std::chrono::duration<double>(now - begin).count();
+  while (duration < time) {
+    now = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration<double>(now - begin).count();
+  }
+}
+
+void compute_by_work(long workload) {
+  for (long i = 0; i < workload; ++i);
+}
