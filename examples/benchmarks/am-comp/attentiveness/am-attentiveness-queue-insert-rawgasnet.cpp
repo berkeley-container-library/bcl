31729b6c690b5146f18631a082b7f73d4ceca8e8
Benjamin Brock <brock@cs.berkeley.edu>
2019-08-23 13:16:14 -0700

    Added raw GASNet-EX version to elucidate issue

---
+++ examples/benchmarks/am-comp/attentiveness/am-attentiveness-queue-insert-rawgasnet.cpp
@@ -0,0 +1,67 @@
+#include <bcl/bcl.hpp>
+#include <queue>
+
+bool thread_run = true;
+
+void service_ampoll() {
+  while (thread_run) {
+    gasnet_AMPoll();
+  }
+}
+
+size_t issued = 0;
+size_t received = 0;
+
+std::queue<int> queue;
+
+void queue_insert_handler(gex_Token_t token, gex_AM_Arg_t value) {
+  queue.push(value);
+  gex_AM_ReplyShort(token, GEX_AM_INDEX_BASE+1, 0);
+}
+
+void reply_handler(gex_Token_t token) {
+  received++;
+}
+
+int main(int argc, char** argv) {
+  size_t num_ams = 50000;
+
+  BCL::init();
+  
+  size_t handler_num = GEX_AM_INDEX_BASE;
+  size_t max_args = gex_AM_MaxArgs();
+
+  gex_AM_Entry_t entry[2];
+  entry[0].gex_index = handler_num++;
+  entry[0].gex_fnptr = (gex_AM_Fn_t) queue_insert_handler;
+  entry[0].gex_flags = GEX_FLAG_AM_SHORT | GEX_FLAG_AM_REQUEST;
+  entry[0].gex_nargs = 1;
+
+  entry[1].gex_index = handler_num++;
+  entry[1].gex_fnptr = (gex_AM_Fn_t) reply_handler;
+  entry[1].gex_flags = GEX_FLAG_AM_SHORT | GEX_FLAG_AM_REPLY;
+  entry[1].gex_nargs = 0;
+
+  int rv = gex_EP_RegisterHandlers(BCL::ep, entry, 2);
+
+  auto thread_ampool = std::thread(service_ampoll);
+
+  srand48(BCL::rank());
+  BCL::barrier();
+
+  for (size_t i = 0; i < num_ams; i++) {
+    size_t remote_proc = lrand48() % BCL::nprocs();
+
+    issued++;
+    int rv = gex_AM_RequestShort(BCL::tm, remote_proc, GEX_AM_INDEX_BASE, 0, BCL::rank());
+  }
+  while (received < issued) {}
+
+  BCL::barrier();
+
+  thread_run = false;
+  thread_ampool.join();
+
+  BCL::finalize();
+  return 0;
+}
