aa67d56f621c70a4e60a8124ea1ef0bab4531adc
Jiakun Yan <jiakunyan1998@gmail.com>
2019-10-21 14:49:50 -0700

    benchmark_arh_histogram: add argument parse; formalize the output

--- examples/benchmarks/arh/benchmark_arh_histogram.cpp
+++ examples/benchmarks/arh/benchmark_arh_histogram.cpp
@@ -21,6 +21,7 @@ void worker() {
   int local_range = 1000;
   int total_range = local_range * (int) ARH::nworkers();
   int num_ops = 100000;
+  int total_num_ops = num_ops * (int) ARH::nworkers();
 
   size_t my_rank = ARH::my_worker_local();
   size_t nworkers = ARH::nworkers();
@@ -57,17 +58,18 @@ void worker() {
   auto end = std::chrono::high_resolution_clock::now();
 
   double duration = std::chrono::duration<double>(time1 - begin).count();
-  ARH::print("Request Phase: agg_size = %lu; duration = %.2lf; overhead = %.2lf us/op\n", agg_size, duration, duration * (1e6 / num_ops));
+  double overhead = duration * (1e6 / num_ops * MAX(agg_size, 1));
+  ARH::print("Request Phase: agg_size = %lu; duration = %.2lf s; overhead   = %.2lf us/op\n", agg_size, duration, overhead);
 
   duration = std::chrono::duration<double>(end - begin).count();
-  ARH::print("Total: agg_size = %lu; duration = %.2lf; throughput = %d ops/s\n", agg_size, duration, (int) (num_ops / duration));
+  ARH::print("Total:         agg_size = %lu; duration = %.2lf s; throughput = %d op/s\n", agg_size, duration, (int) (num_ops / duration));
 }
 
 int main(int argc, char** argv) {
   // one process per node
   cxxopts::Options options("ARH Benchmark", "Benchmark of ARH system");
   options.add_options()
-      ("size", "Aggregation size", cxxopts::value<size_t>(0))
+      ("size", "Aggregation size", cxxopts::value<size_t>())
       ;
   auto result = options.parse(argc, argv);
   agg_size = result["size"].as<size_t>();
@@ -77,7 +79,7 @@ int main(int argc, char** argv) {
   ARH::init(15, 16);
   mObjects.init();
   if (use_agg) {
-    ARH::set_agg_size(agg_size);
+    agg_size = ARH::set_agg_size(agg_size);
   }
 
   ARH::run(worker);
