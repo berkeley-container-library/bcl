1acf17e10624f71fdc8b05acc5a4f19ac9ef9b7e
Benjamin Brock <brock@cs.berkeley.edu>
2021-03-15 22:11:04 -0700

    Add placeholders for arbroadcast for SHMEM backend

--- bcl/backends/shmem/comm.hpp
+++ bcl/backends/shmem/comm.hpp
@@ -110,6 +110,17 @@ inline T broadcast(const T &val, uint64_t root) {
   }
 }
 
+template <typename T>
+inline auto arbroadcast(T* val, uint64_t root, size_t size, const BCL::Team& team) {
+  throw debug_error("BCL arbroadcast(): Has not been implemented on SHMEM backend.");
+  return BCL::request(gex_Event_t());
+}
+
+template <typename T, typename Allocator>
+inline void broadcast(std::vector<T, Allocator>& val, uint64_t root, const BCL::Team& team) {
+  throw debug_error("BCL broadcast() (with teams): Has not been implemented on SHMEM backend.");
+}
+
 template <typename T, typename BinaryOp>
 inline T allreduce(const T &val, BinaryOp op) {
   T rv = val;
