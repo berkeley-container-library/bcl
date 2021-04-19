760ec0d8cc5b0dd513ea10ce7248c5ace89ca38b
Benjamin Brock <brock@cs.berkeley.edu>
2021-04-19 12:23:32 -0700

    Fix example

--- examples/experimental/rpc/test_queue.cpp
+++ examples/experimental/rpc/test_queue.cpp
@@ -5,19 +5,18 @@
 
 int main(int argc, char** argv) {
   BCL::init(256, true, true);
-  BCL::CircularQueue<std::string> queue(0, 100);
+  BCL::CircularQueue<std::string> queue(0, 10*BCL::nprocs());
 
   if (BCL::rank() != 0) {
-    for (size_t i = 0; i < 100; i++) {
+    for (size_t i = 0; i < 10; i++) {
       queue.push(std::string("this is the ") + std::to_string(i) + "th long string from " + std::to_string(BCL::rank()));
     }
   } else {
-    for (size_t i = 0; i < (BCL::nprocs()-1)*100; i++) {
+    for (size_t i = 0; i < (BCL::nprocs()-1)*10; i++) {
       std::string buf;
-      bool success = queue.pop(buf);
-      if (success) {
-        printf("Popped \"%s\"\n", buf.c_str());
-      }
+      bool success = false;
+      while (!success) { success = queue.pop(buf); usleep(100); }
+      printf("Popped \"%s\"\n", buf.c_str());
     }
   }
 
