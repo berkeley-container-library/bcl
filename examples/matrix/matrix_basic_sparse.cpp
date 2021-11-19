c6f4eed7c85a659137204e3ec9da35e51eebc2ef
Benjamin Brock <brock@cs.berkeley.edu>
2021-11-18 21:40:58 -0800

    Add examples

--- examples/matrix/matrix_basic_sparse.cpp
+++ examples/matrix/matrix_basic_sparse.cpp
@@ -13,21 +13,10 @@ float multiply_by_two(float x) {
 int main(int argc, char** argv) {
   BCL::init();
 
-  BCL::DMatrix<float> a({8, 8});
-  BCL::DMatrix<float> b({8, 8});
+  BCL::SPMatrix<float> a("1138_bus.mtx");
+  BCL::DMatrix<float> b({a.shape()[1], 8});
 
-  a = 1;
   b = 1;
-
-  BCL::print("A matrix:\n");
-  a.print();
-
-  a.apply_inplace(multiply_by_two);
-
-  BCL::print("A matrix after applying \"multiply_by_two\":\n");
-  a.print();
-
-  b.apply_inplace([](float x) { return x + 12; });
 
   auto c = a.dot(b);
 
