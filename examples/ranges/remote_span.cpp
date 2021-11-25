35898fa395238f502d4293bd9b641a696ae34ef2
Benjamin Brock <brock@cs.berkeley.edu>
2021-11-24 20:43:43 -0800

    Started new branch for changing teams

--- examples/ranges/remote_span.cpp
+++ examples/ranges/remote_span.cpp
@@ -1,13 +1,22 @@
 #include <bcl/bcl.hpp>
 #include <iostream>
+#include <limits>
+#include <execution>
+#include <bcl/core/teams.hpp>
+#include <ranges>
 
 #ifndef CPP20
-#include <limits>
 namespace std {
 inline constexpr std::size_t dynamic_extent = std::numeric_limits<std::size_t>::max();
+
+struct identity {
+  template <typename T>
+  constexpr T&& operator()( T&& t ) const noexcept { std::forward<T>(t); }
+};
 }
 #else
 #include <span>
+#include <functional>
 #endif
 
 template <typename T,
@@ -74,6 +83,22 @@ public:
     return data() + size();
   }
 
+  T* local_begin() const noexcept {
+    if (BCL::rank() == data().rank) {
+      return begin().local();
+    } else {
+      return nullptr;
+    }
+  }
+
+  T* local_end() const noexcept {
+    if (BCL::rank() == data().rank) {
+      return end().local();
+    } else {
+      return nullptr;
+    }
+  }
+
   constexpr reference front() const {
     return data();
   }
@@ -134,6 +159,128 @@ private:
   remote_span_storage_impl_<T, Extent> storage_;
 };
 
+namespace bcl {
+inline constexpr std::size_t block_stride = std::numeric_limits<std::size_t>::max();
+namespace execution {
+
+template <typename LocalExecutionPolicy = std::execution::parallel_unsequenced_policy>
+class sequential_policy {
+public:
+  constexpr sequential_policy(std::size_t rank = 0, LocalExecutionPolicy local_policy = std::execution::par_unseq)
+    : rank_(rank), local_policy_(local_policy) {}
+
+  constexpr std::size_t rank() const noexcept {
+    return rank_;
+  }
+
+  constexpr std::size_t stride() const noexcept {
+    return bcl::block_stride;
+  }
+
+  constexpr std::size_t stride(std::size_t count) const noexcept {
+    return count;
+  }
+
+  constexpr BCL::RangeTeam team() const noexcept {
+    return BCL::RangeTeam(rank_, rank_+1);
+  }
+
+  constexpr LocalExecutionPolicy local_policy() const noexcept {
+    return local_policy_;
+  }
+
+private:
+  std::size_t rank_;
+  LocalExecutionPolicy local_policy_;
+};
+
+template <typename TeamType = BCL::WorldTeam,
+          typename LocalExecutionPolicy = std::execution::parallel_unsequenced_policy>
+class parallel_policy {
+public:
+  constexpr parallel_policy(std::size_t stride = bcl::block_stride,
+                            TeamType&& team = TeamType(),
+                            LocalExecutionPolicy local_policy = std::execution::par_unseq)
+    : stride_(stride), team_(team), local_policy_(local_policy) {}
+
+  constexpr std::size_t stride() const noexcept {
+    return stride_;
+  }
+
+  std::size_t stride(std::size_t count) const noexcept {
+    if (stride() == bcl::block_stride) {
+      return (count + team().nprocs() - 1) / team().nprocs();
+    } else {
+      return stride();
+    }
+  }
+
+  constexpr const TeamType& team() const noexcept {
+    return team_;
+  }
+
+  constexpr LocalExecutionPolicy local_policy() const noexcept {
+    return local_policy_;
+  }
+
+private:
+  std::size_t stride_;
+  TeamType team_;
+  LocalExecutionPolicy local_policy_;
+};
+
+template <typename LocalExecutionPolicy = std::execution::parallel_unsequenced_policy>
+class parallel_local_policy {
+public:
+  constexpr parallel_local_policy(LocalExecutionPolicy local_policy = std::execution::par_unseq)
+    : local_policy_(local_policy) {}
+
+  constexpr LocalExecutionPolicy local_policy() const noexcept {
+    return local_policy_;
+  }
+private:
+  LocalExecutionPolicy local_policy_;
+};
+
+inline constexpr sequential_policy seq = sequential_policy();
+inline constexpr parallel_policy par = parallel_policy();
+inline constexpr parallel_local_policy par_local = parallel_local_policy();
+
+}
+}
+
+// Limited to random access ranges
+// Not locality-aware
+template <typename ExecutionPolicy, typename R, typename Fun, typename Proj = std::identity>
+void for_each(ExecutionPolicy policy, R&& r, Fun f, Proj proj = {}) {
+  if constexpr(!std::is_same_v<ExecutionPolicy, bcl::execution::parallel_local_policy<>>) {
+    if (policy.team().in_team()) {
+      std::size_t stride = policy.stride(r.size());
+      std::size_t chunk_size = stride*policy.team().nprocs();
+      std::size_t num_chunks = (r.size() + chunk_size - 1) / chunk_size;
+
+      for (size_t chunk = 0; chunk < num_chunks; chunk++) {
+        std::size_t offset = chunk*chunk_size + policy.team().rank()*stride;
+        std::size_t size = std::min(stride, r.size() - offset);
+        // XXX: apparent bug in STL impl. when:
+        //  1) passing policy as rvalue reference
+        //  2) using random_access_iterator, but not continguous_iterator
+        // Copying to new local variable as workaround.
+        auto local_policy = policy.local_policy();
+        std::for_each(local_policy, r.begin() + offset, r.begin() + offset + size, f);
+      }
+    }
+  } else {
+    auto local_policy = policy.local_policy();
+    std::for_each(policy.local_policy(), r.local_begin(), r.local_end(), f);
+  }
+}
+
+template <std::ranges::range R, typename Fun, typename Proj = std::identity>
+void for_each(R&& r, Fun f, Proj proj = {}) {
+  for_each(bcl::execution::par_local, std::forward<R>(r), f, proj);
+}
+
 int main(int argc, char** argv) {
   BCL::init();
 
@@ -181,6 +328,26 @@ int main(int argc, char** argv) {
     }
     std::cout << std::endl;
   }
+
+  BCL::barrier();
+  fflush(stdout);
+  fflush(stderr);
+  usleep(100);
+  BCL::barrier();
+
+  for_each(bcl::execution::parallel_policy(1),
+           span, [](auto f) { printf("(%lu) Hello, fam! got %d\n", BCL::rank(), (int) f); });
+
+/*
+  BCL::barrier();
+  fflush(stdout);
+  fflush(stderr);
+  usleep(100);
+  BCL::barrier();
+
+  for_each(bcl::execution::par_local,
+           span, [](auto f) { printf("(%lu) Hello, fam! got %d\n", BCL::rank(), (int) f); });
+           */
 
   BCL::finalize();
   return 0;
